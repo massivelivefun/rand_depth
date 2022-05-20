16bcdc0b59f38f8a0cd31f8e4744617a60cef539
Matthew Lance Fuller <matthewlancefuller@gmail.com>
2022-05-19 19:06:09 -0700

    Add more max depth tests

--- tests/max_depth_test.py
+++ tests/max_depth_test.py
@@ -1,4 +1,4 @@
-from src.rand_depth.node import Node
+from src.rand_depth.node import T, Node
 from src.rand_depth.tree_operations import TreeOperations
 import types
 from typing import TypeVar
@@ -7,7 +7,30 @@ import unittest
 
 class TreeOperationTest(unittest.TestCase):
     def setUp(self) -> None:
-        self.tree_op_class_inst = TreeOperations()
+        # left heavy tree
+        l_left2 = Node("left2", None, None)
+        l_left = Node("left", l_left2, None)
+        l_right = Node("right", None, None)
+        self.left_heavy_tree = Node("root", l_left, l_right)
+
+        # right heavy tree
+        r_right2 = Node("right2", None, None)
+        r_right = Node("right", r_right2, None)
+        r_left = Node("left", None, None)
+        self.right_heavy_tree = Node("root", r_left, r_right)
+
+        # single node tree
+        self.root_tree = Node("root", None, None)
+
+        # An instance of TreeOperations
+        self.tree_op_class_instance = TreeOperations()
+
+    def test_max_depth_3_bunch_of_times(self) -> None:
+        num_of_trees = 9999
+        max_depth = 2
+        for _ in range(0, num_of_trees):
+            root = TreeOperations.gen_tree(max_depth)
+            self.assertEqual(TreeOperations.max_depth(root), max_depth)
 
     def test_max_depth_3_bunch_of_times(self) -> None:
         num_of_trees = 9999
@@ -32,11 +55,14 @@ class TreeOperationTest(unittest.TestCase):
 
     def test_gen_tree_is_static_method(self) -> None:
         self.assertIsInstance(
-            self.tree_op_class_inst.gen_tree, types.FunctionType)
+            self.tree_op_class_instance.gen_tree, types.FunctionType)
 
     def test_max_depth_is_static_method(self) -> None:
         self.assertIsInstance(
-            self.tree_op_class_inst.max_depth, types.FunctionType)
+            self.tree_op_class_instance.max_depth, types.FunctionType)
 
-    # def test_a_is_typedef_object():
+    # def test_t_is_typedef_object():
     #     assert isinstance(A, TypeVar)
+
+    def test_t_typevar_name_is_t(self) -> None:
+        self.assertEqual(T.__name__, 'T')
