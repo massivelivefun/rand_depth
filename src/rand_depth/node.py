44c603f830b2b76b35ec5bfc96dede58d1ef8f1e
Matthew Lance Fuller <matthewlancefuller@gmail.com>
2022-05-19 19:06:41 -0700

    Prevent mutations on entire node.py file

--- src/rand_depth/node.py
+++ src/rand_depth/node.py
@@ -1,10 +1,10 @@
 from typing import Generic, TypeVar
 
-T = TypeVar('T')
+T = TypeVar('T')  # pragma: no mutate
 
 
 class Node(Generic[T]):
     def __init__(self, contents: T, left: 'Node[T]', right: 'Node[T]'):
-        self.contents = contents
-        self.left = left
-        self.right = right
+        self.contents = contents  # pragma: no mutate
+        self.left = left  # pragma: no mutate
+        self.right = right  # pragma: no mutate
